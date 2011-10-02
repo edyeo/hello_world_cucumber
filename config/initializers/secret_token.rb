2a8767be600e858bbf2ae43643732204026466c4
AdamT <atal421@yahoo.com>
2011-10-02 00:57:27 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorldCucumber::Application.config.secret_token = '49950a883278897ab62b7499ebf843b183bd4d31d061b411d1549e8a65c80a8fcd77f925344804c7f010e048cfd10629cb19562acb65318f4733ad38a80113bd'
